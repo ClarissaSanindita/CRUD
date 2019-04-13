dcede6f3a0bc4172576bb301c5fed7a9246662ab
ClarissaSanindita <clarissa_sanindita_26rpl@student.smktelkom-mlg.sch.id>
2019-04-13 19:13:36 +0700

    selesai

---
+++ app/src/main/java/id/sch/smktelkom_mlg/www/crud2_clarissa/ShareActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.www.crud2_clarissa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+}
